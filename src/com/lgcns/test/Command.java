7a38c26723b6e05a5cc5b63af97a342fc9de6ede
jaewon801120 <jaewon801120@gmail.com>
2026-04-28 01:46:31 +0900

    sample exercise

---
+++ src/com/lgcns/test/Command.java
@@ -0,0 +1,10 @@
+package com.lgcns.test;
+
+import com.google.gson.Gson;
+
+public class Command {
+
+	public String toJson(Gson gson) {
+		return gson.toJson(this);
+	}
+}
---
+++ src/com/lgcns/test/RunManager.java
@@ -0,0 +1,9 @@
+package com.lgcns.test;
+
+public class RunManager {
+
+	public static void main(String[] args) throws Exception {
+		Solution solution = new Solution();
+		solution.run();
+	}
+}
